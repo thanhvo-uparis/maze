39c51cbdf18118afa054f4c39812b1aa58214863
thanh vo.u-paris <72513778+thanhvo-uparis@users.noreply.github.com>
2022-03-20 23:43:09 +0100

    initiale complete

--- src/initial.py
+++ src/initial.py
@@ -1,45 +1,32 @@
 import queue
 
-with open ("./maps/rect_01.map", 'r') as map:
-    matrice = map.read().split("\n")
-
-matrix = []
-
-start_x = 0
-start_y = 0
-end_x = 0
-end_y = 0
-
-x = 0
-
-for line in matrice:
-    y = 0
-    if x == 0 or x > len(matrix) - 1:
-        matrix.append([])
-    for value in line:
-        if value == '1':
-            start_x = x
-            start_y = y
-
-        if value == '2':
-            end_x = x
-            end_y = y
-
-        matrix[x].append(value)
-        y += 1
-    x += 1
-
 
 def createMaze():
     maze = []
-    for i in range(0, y - 1):
-        maze.append(matrix[i])
+    maze.append(["*", "*", "*", "*", "*", "1", "*"])
+    maze.append(["*", " ", " ", " ", "*", " ", "*"])
+    maze.append(["*", " ", "*", " ", "*", " ", "*"])
+    maze.append(["*", " ", "*", " ", " ", " ", "*"])
+    maze.append(["*", " ", "*", "*", "*", " ", "*"])
+    maze.append(["*", " ", " ", " ", "*", " ", "*"])
+    maze.append(["*", "*", "*", "2", "*", "*", "*"])
+
     return maze
 
-pt_start = (start_x, start_y)
-print(pt_start)
 
-createMaze()
+def createMaze2():
+    maze = []
+    maze.append(["*", "*", "*", "*", "*", "1", "*", "*", "*"])
+    maze.append(["*", " ", " ", " ", " ", " ", " ", " ", "*"])
+    maze.append(["*", " ", "*", "*", " ", "*", "*", " ", "*"])
+    maze.append(["*", " ", "*", " ", " ", " ", "*", " ", "*"])
+    maze.append(["*", " ", "*", " ", "*", " ", "*", " ", "*"])
+    maze.append(["*", " ", "*", " ", "*", " ", "*", " ", "*"])
+    maze.append(["*", " ", "*", " ", "*", " ", "*", "*", "*"])
+    maze.append(["*", " ", " ", " ", " ", " ", " ", " ", "*"])
+    maze.append(["*", "*", "*", "*", "*", "*", "*", "2", "*"])
+
+    return maze
 
 def printMaze(maze, path=""):
     for x, pos in enumerate(maze[0]):
@@ -101,7 +88,6 @@ def valid(maze, moves):
 
 
 def findEnd(maze, moves):
-    start = 0
     for x, pos in enumerate(maze[0]):
         if pos == "1":
             start = x
